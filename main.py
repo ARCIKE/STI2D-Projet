7e8033519fc92b5b62cfe398a907be9bd466bd99
Alex <64358227+ARCIKE@users.noreply.github.com>
2022-06-01 06:35:38 +0000

    Update

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from replit import db
 import random
 
-app = Flask(  # Create a flask app
+app = Flask(
 	__name__,
-	template_folder='templates',  # Name of html file folder
-	static_folder='static'  # Name of directory for static files
+	template_folder='templates',
+	static_folder='static'
 )
-
 
 @app.route('/')
 def home_page():
     return render_template("index.html")
-  
+ 
 @app.route('/path/<longa>/<lata>/<longb>/<latb>')
 def coord(longa,lata,longb,latb):
     return render_template("path.html", longa=longa, lata=lata, longb=longb, latb=latb)
 
-if __name__ == "__main__":  # Makes sure this is the main process
-	app.run( # Starts the site
-		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+if __name__ == "__main__":
+	app.run(
+		host='0.0.0.0',
+		port=random.randint(2000, 9000)
 	)
-
